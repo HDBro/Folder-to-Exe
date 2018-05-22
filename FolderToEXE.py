a4578621315cb4dbbf31880612136c1862f5eaeb
HDBro <DoodledTiger@gmail.com>
2018-05-21 19:16:01 -0700

    Lots of bugs...in beta1
    lol, 90% chance it wont work  right now. If you want you can try to fix it!

---
+++ FolderToEXE.py
@@ -0,0 +1,132 @@
+import os
+import time
+from random import randint
+
+print("RAMing memory voids")
+
+print("booting...")
+time.sleep(0.3)
+print()
+print("create a backup folder incase any bugs or glitches")
+time.sleep(1)
+
+print()
+index = input("index (run file): ")
+
+print("booting...")
+def convert():
+    print("converting py to exe...")
+    os.rename("name_me.py", "name_me.exe")
+    print()
+    print()
+    print()
+    print()
+    print("---your exe file has been created---")
+    print("do not move it from its folder. Create a shortcut instead")
+    print()
+    time.sleep(20)
+
+time.sleep(0.2)
+print("booting...")
+def vertify(x):
+    if "." in x:
+        print("file succesfully verified...")
+        print()
+        run(index)
+    else:
+        print("the file cannot be turned into ")
+
+time.sleep(0.2)
+print("booting...")
+def count_files():
+    print("returning os.scandir")
+    h = len([1 for x in list(os.scandir()) if x.is_file()])
+
+time.sleep(0.2)
+print("booting...")
+def RAMsetup():
+    try:
+        f.close()
+    except:
+        Exception
+
+    try:
+        c.close()
+    except:
+        Exception
+
+    f = open("name_me.py", "a")
+    f.write("time.sleep(1.4)/n")
+    f.write("os.system('" + index + "')/n")
+    f.write("time.sleep(1.3)/n")
+    ran = randint(0,999)
+    f.write("if not os.path.exists('C:/exe" + ran + "'):/n")
+    f.write("   os.makedir('C:/exe" ran "')/n")
+    f.write("os.replace('C:/FILEtoEXE/', 'C:/ran/')/n")
+    f.close()
+    convert()
+
+time.sleep(0.2)
+print("booting...")
+def dataInput(i):
+    try:
+        f.close()
+    except:
+        Exception
+
+    print("getting file lengths...")
+    print(".")
+    print(".")
+    count = h.count_files()
+    print(count)
+    print(".")
+    print(".")
+    f = open("name_me.py", "a")
+    print("making quick RAM accessable...")
+    print()
+    time.sleep(2)
+    safe = True
+    print("-----WARNING - Do not close window! - it will mess things up-----")
+    time.sleep(4)
+    print("starting operation...")
+    time.sleep(1)
+    st = 1
+    while safe = True:
+        print("FILE++ " + st)
+        st = st + 1
+        try:
+            print("adding line-" + count[st - 1])
+            c = open(count[st - 1], "r")
+            f.write("f = open('" + count[st - 1] + "', 'w')/n")
+            f.write("f.write('" + c + "')/n")
+            f.write("f.close()/n")
+            c.close()
+        except:
+            f.close()
+            safe = False
+            
+            
+
+    print("succesfully copied files to exe data base...")
+    time.sleep(0.2)
+    RAMsetup()
+    
+
+time.sleep(0.2)
+print("booting...")
+def run(indexx):
+    
+    f = open("name_me.py", "w")
+    f.write("import os/n")
+    f.write("import time/n")
+    
+    f.close()
+    #rename to exe-
+    
+    
+    dataInput(indexx)
+
+time.sleep(0.3)
+print("booting complete")
+
+
--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# Folder to Exe
+made by "HDBro (Hudson)" the hacker
+
+-----------------------------------------------
+
+code created  on the date 5/21/2018
+
+
+-----------------------------------------------
+
+How to Use---
+
+requirments:
+	python 3.6.3
+	
+Setup:
+	Move the folder "FOLDERtoEXE" into "C:/"
+	Go to the folder of your choosings (the folder you want to convert) and make a copy of it just in case ;)
+	Take all the files out of the folder you want to convert and put them into C:/FOLDERtoEXE/
+
+USAGE:
+	run "FolderToEXE.py"
+	the console will ask you for an index (the script that starts the folder (Ex. the index can be- main.bat))
+	type in the index (Ex. main.bat) then press enter
+	in less that a minute (maybe), your exe file will be created. 
+
+After the USAGE:
+	Once you have ran the console, go to C:/ and a folder will apear. The folder will me named exe then random numbers
+	(Ex. C:/exe193)
+	Once you open the folder you will see the exe file looking back at you, BUT WAIT...
+	You can not move the exe files direcotry. Instead, make a shortcut of the file.
